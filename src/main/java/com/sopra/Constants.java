8a1e5d5f7ab46867a5e796b6bb53c7a4eb806373
Jordan <jordan.priam@hotmail.com>
2017-11-20 13:19:22 +0100

    modify user

--- src/main/java/com/sopra/Constants.java
+++ src/main/java/com/sopra/Constants.java
@@ -2,19 +2,13 @@ package com.sopra;
 
 public class Constants {
 
-<<<<<<< HEAD
+
 	public static String NOT_SAME_PASSWORD = "Password does not match the confirm password";
 	public static String BAD_PASSWORD_OR_LOGIN_ALERT = "Error: bad password or login";
 	public static String ALERT_ATTRIBUTE="alert";
 	public static String CONNECTED_USER_ATTRIBUTE="connectedUser";
 	public static String LOGIN_IS_USED ="Login already exists";
 	public static String EMPTY_ATTRIBUTE = "Password or login is empty";
-=======
-	public static String NOT_SAME_PASSWORD_ALERT = "Error: password not confirmed";
-	public static String BAD_PASSWORD_OR_LOGIN_ALERT = "Error: bad password or login";
-	public static String ALERT_ATTRIBUTE="alert";
-	public static String CONNECTED_USER_ATTRIBUTE="connectedUser";
-	public static String LOGIN_ALREADY_USED_ALERT = "Error: login is already used";
->>>>>>> 658e0a7c2745fe5c0e5073b0f7af1c1f9527b338
+
 	
 }
