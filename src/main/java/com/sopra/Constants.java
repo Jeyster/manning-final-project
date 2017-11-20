9ebe17aaa5c0059c30ec0eb2315d3d670619b19a
Emmanuel <emmanuel.fabre.31@gmail.com>
2017-11-20 13:20:48 +0100

    register not empty fields

--- src/main/java/com/sopra/Constants.java
+++ src/main/java/com/sopra/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
 	public static String ALERT_ATTRIBUTE="alert";
 	public static String CONNECTED_USER_ATTRIBUTE="connectedUser";
 	public static String LOGIN_ALREADY_USED_ALERT = "Error: login is already used";
-	
+	public static String EMPTY_FIELD_ALERT = "Error: login and password can't be empty";	
 }
